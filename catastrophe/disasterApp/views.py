0f543c55d868b1b634c9d0ad68576191590c1dd8
Akil Arif <akil_ibrahim@yahoo.co.in>
2018-03-10 01:53:26 +0530

    Modify views.py to include register and user_page

--- catastrophe/disasterApp/views.py
+++ catastrophe/disasterApp/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def login_register(request):
     return render(request, 'disasterApp/login_register.html', {})
+
+def user_page(request):
+	return render(request, 'disasterApp/user_page.html', {})
+
+def register(request):
+	return render(request, 'disasterApp/register.html', {})
