6aff8c9a6bafdfc55d7a6c3c2676975a2c810421
Akil Arif <akil_ibrahim@yahoo.co.in>
2018-03-10 07:45:54 +0530

    Modify login_register.html, urls.py and include the skeleton of file user_page.html

--- catastrophe/disasterApp/urls.py
+++ catastrophe/disasterApp/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.login_register, name='login_register'),
+    url(r'^user_page/', views.user_page, name='user_page'),
+    url(r'^register/', views.register, name='register'),
 ]
